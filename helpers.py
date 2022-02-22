4a4a137367c432fdede1a7807b98d3447f58e947
artem.olkhovskyi <artem.olkhovskyi@avid.com>
2022-02-23 01:01:27 +0200

    fixup

--- helpers.py
+++ helpers.py
@@ -2,12 +2,14 @@ pobut = dict()
 pobut['15-60'] = .00097
 pobut['15-19'] = .00072
 pobut['20-24'] = .00110
+pobut['45-49'] = .001
 pobut['60-64'] = .0014
 
 genetic = dict()
 pobut['15-60'] = .03800
 genetic['15-19'] = .00030
 genetic['20-24'] = .00040
+genetic['45-49'] = .0048
 genetic['60-64'] = .025
 
 living_place = {
@@ -39,8 +41,12 @@ hobbies['student'] = 5 * (10 ** (-8))
 hobbies['driving'] = .000001
 hobbies['skiing'] =  .0000003
 hobbies['driver-professional'] = .0000003
+hobbies['potter'] = .00000025
+hobbies['speliolog'] = .000027
 
 habits = dict()
 
 habits['smoking'] = .008
 habits['drinking'] = .000212
+habits['car-accident'] = .00019
+habits['drowning'] = .000091
--- lab1.py
+++ lab1.py
@@ -1,8 +1,9 @@
 from human import Human
 
-Artem = Human('15-19', 'male', 'city', 'student', 'driving', 'drinking', 2 * 356)
-someone = Human('60-64', 'male', 'city', 'driver-professional', 'skiing', 'drinking', 150)
+if __name__ == '__main__':
+    # Artem = Human('15-19', 'male', 'city', 'student', 'driving', 'drinking', 2 * 356)
+    # someone = Human('60-64', 'male', 'city', 'driver-professional', 'skiing', 'drinking', 150)
+    someone = Human('45-49', 'male', 'village', 'potter', 'speliolog', 'drowning')
 
-
-Artem.show_risks()
-someone.show_risks()
+    # Artem.show_risks()
+    someone.show_risks()
